b15bb20d5c73955264c2116afdd491e2d68a01f0
Glenn Montera <glenn.montera@gmail.com>
2023-11-13 22:56:12 -0500

    fixed styling

--- client/src/pages/ArtistPage/ArtistPage.js
+++ client/src/pages/ArtistPage/ArtistPage.js
@@ -1,9 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import "./ArtistPage.scss";
-import AlbumCover from "../../components/AlbumCover/AlbumCover";
 
 const serverUrl = process.env.REACT_APP_SERVER_URL;
 
