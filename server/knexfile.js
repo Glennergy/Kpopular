27647494e90e7edd6bbdbde379149c8571d65498
Glenn Montera <glenn.montera@gmail.com>
2023-11-16 19:36:29 -0500

    changeds knexfile and env file

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 module.exports = {
   client: "mysql",
   connection: {
-    host: "127.0.0.1",
-    user: "root",
-    password: "rootroot",
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USERNAME,
+    password: process.env.SQL_SECRET,
     database: "kpopular",
     charset: "utf8",
   },
